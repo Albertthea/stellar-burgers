a4cfd7738974901d1280ec90f4b634bddbab85a9
Iana Spiridonova <iana.spiridonova01@gmail.com>
2025-04-01 20:36:56 +0200

    model ingredients

--- src/components/feed-info/feed-info.tsx
+++ src/components/feed-info/feed-info.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 
 import { TOrder } from '@utils-types';
 import { FeedInfoUI } from '../ui/feed-info';
+import { useSelector } from '@store';
+import { getFeedStateSelector } from '@slices';
 
 const getOrders = (orders: TOrder[], status: string): number[] =>
   orders
@@ -10,7 +12,7 @@ const getOrders = (orders: TOrder[], status: string): number[] =>
     .slice(0, 20);
 
 export const FeedInfo: FC = () => {
-  /** TODO: взять переменные из стора */
+  const ordersState = useSelector(getFeedStateSelector);
   const orders: TOrder[] = [];
   const feed = {};
 
--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useParams } from 'react-router-dom';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
+  const ingridientId = useParams().id;
   const ingredientData = null;
 
   if (!ingredientData) {
