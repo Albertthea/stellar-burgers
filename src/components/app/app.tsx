fdad8a71410a6ddea542f9e7dde8e52409b53db2
Iana Spiridonova <iana.spiridonova01@gmail.com>
2025-04-28 17:54:30 +0200

    modal in route added

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,64 +1,74 @@
-import {
-  Routes,
-  Route,
-  Outlet,
-  useLocation,
-  useNavigate
-} from 'react-router-dom';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import {
   ConstructorPage,
   Feed,
-  Login,
-  Register,
   ForgotPassword,
-  ResetPassword,
+  Login,
+  NotFound404,
   Profile,
   ProfileOrders,
-  NotFound404
+  Register,
+  ResetPassword
 } from '@pages';
-import { AppHeader, Modal, IngredientDetails, OrderInfo } from '@components';
-import { RouteProtected } from '../route-protected';
 import '../../index.css';
 import styles from './app.module.css';
-
-const Layout = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <Outlet />
-  </div>
-);
+import {
+  AppHeader,
+  Modal,
+  OrderInfo,
+  IngredientDetails,
+  RouteProtected
+} from '@components';
+import { useDispatch } from '../../services/store';
+import { getIngredientsThunk, selectUserState, getUserThunk } from '@slices';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Preloader } from '../ui/preloader';
 
 const App = () => {
-  const location = useLocation();
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const background = location.state?.background;
+  const location = useLocation();
+  const backgroundLocation = location.state?.background;
+  const userLoading = useSelector(selectUserState).isLoading;
+
+  useEffect(() => {
+    dispatch(getUserThunk());
+    dispatch(getIngredientsThunk());
+  }, [dispatch]);
+
+  if (userLoading) {
+    return <Preloader />;
+  }
 
   return (
-    <>
-      <Routes location={background || location}>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<ConstructorPage />} />
-          <Route path='feed' element={<Feed />} />
-          <Route element={<RouteProtected forAuthorized={false} />}>
-            <Route path='login' element={<Login />} />
-            <Route path='register' element={<Register />} />
-            <Route path='forgot-password' element={<ForgotPassword />} />
-            <Route path='reset-password' element={<ResetPassword />} />
-          </Route>
-
-          <Route element={<RouteProtected forAuthorized />}>
-            <Route path='profile' element={<Profile />} />
-            <Route path='profile/orders' element={<ProfileOrders />} />
-          </Route>
-
-          <Route path='*' element={<NotFound404 />} />
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes location={backgroundLocation || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        <Route path='/feed/:number' element={<OrderInfo />} />
+        <Route element={<RouteProtected forAuthorized={false} />}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
         </Route>
+
+        <Route element={<RouteProtected forAuthorized />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/orders' element={<ProfileOrders />} />
+          <Route path='/profile/orders/:number' element={<OrderInfo />} />
+        </Route>
+
+        <Route path='*' element={<NotFound404 />} />
       </Routes>
 
-      {background && (
+      {backgroundLocation && (
         <Routes>
           <Route
-            path='feed/:number'
+            path='/feed/:number'
             element={
               <Modal
                 title={`#${location.pathname.match(/\d+/)?.[0]}`}
@@ -69,7 +79,7 @@ const App = () => {
             }
           />
           <Route
-            path='ingredients/:id'
+            path='/ingredients/:id'
             element={
               <Modal title='Детали ингредиента' onClose={() => navigate(-1)}>
                 <IngredientDetails />
@@ -78,7 +88,7 @@ const App = () => {
           />
           <Route element={<RouteProtected forAuthorized />}>
             <Route
-              path='profile/orders/:number'
+              path='/profile/orders/:number'
               element={
                 <Modal
                   title={`#${location.pathname.match(/\d+/)?.[0]}`}
@@ -91,7 +101,7 @@ const App = () => {
           </Route>
         </Routes>
       )}
-    </>
+    </div>
   );
 };
 
