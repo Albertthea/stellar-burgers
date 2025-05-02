8af6e8b4ebfbe53ad2b1ead3b8653de398e2aac1
Iana Spiridonova <iana.spiridonova01@gmail.com>
2025-05-02 21:56:42 +0200

    register added

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,13 +1,27 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { useDispatch } from '../../services/store';
+import { useNavigate } from 'react-router-dom';
+import { registerUserThunk } from '@slices';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorText, setErrorText] = useState('');
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(registerUserThunk({ name: userName, email, password }))
+      .unwrap()
+      .then(() => navigate('/login'))
+      .catch((err) => {
+        console.error(err);
+        setErrorText('Ошибка регистрации. Проверьте данные.');
+      });
   };
 
   return (
