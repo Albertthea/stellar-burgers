2231680adce6fdad235299b55cd56dc6c636a80e
Iana Spiridonova <iana.spiridonova01@gmail.com>
2025-05-02 21:42:42 +0200

    login dispatch made

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,27 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import { useNavigate } from 'react-router-dom';
+import { loginUserThunk, selectUserError } from '@slices';
 
 export const Login: FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const errorText = useSelector(selectUserError);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(loginUserThunk({ email, password }))
+      .unwrap()
+      .then(() => {
+        navigate('/');
+      })
+      .catch((err) => {
+        console.error('[Login error]', err);
+      });
   };
 
   return (
